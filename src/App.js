ca25a745df36595c9a775f59ae82761aa5afe495
achiara311 <47039507+achiara311@users.noreply.github.com>
2022-08-08 00:07:32 -0400

    Fixed UI on app.js
    UI is now working and axios is now getting data back from the api

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import './App.css'
 
 class App extends Component {
   state = {
     users: [],
-    loading: false //when fetching data, it will be loading:true
-    //that way in our UI, we can say if the data isnt loaded, then lets show a loading spinner.
+    loading: false //true if fetch is still processing and spinner is going
+    //false if the data has been fetched and has arrived from api
   }
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false })
 
-  //to make HTTP request to api, need to use componentDidMount() to initialize/make request 
-  //& Axios to do fetch     npm i axios
-  async componetDidMount() {
-    this.setState({ loading: true });
-
-    const res = await axios
-      .get('https://api.github.com/users/v3');
-
-    this.setState({ users: res.data, loading: false }) //we get data at this point,
-      //then make loading false again
-
-      (console.log(res.data)); //github API
   }
-  // render() is a lifecycle method and runs at a certain point when the component is
-  //loaded. It renders the output and is **REQUIRED.
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />
         <div className='container'>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-
-
-    render() {
+const Users = ({ users, loading }) => {
+    if (loading) {
+        return <Spinner />
+    } else {
         return (//map is a high order array method that takes a function/callback function
             //function has a parameter that represents each user called user
             <div style={userStyle}>
-                {this.props.users.map(user => (
-                    <UserItem key={user.id} user={user} /> //myUser prop from UserItem line 7
-                    //passing user in as a prop into UserItem
+                {users.map(user => (
+                    <UserItem key={user.id} user={user} /> //user prop from UserItem line 7
+                    //passing userdetails (login, avatar_url, html_url link) in as a prop into UserItem
                 ))}</div>
-        )
+        );
     }
+
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
